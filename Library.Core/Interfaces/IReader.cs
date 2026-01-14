d027a243b4d122bd30040006cde5ed37e532f0b7
Illua Komolov (Team Lead) <illua.komolov@gmail.com>
2026-01-14 16:49:26 +0200

    Створення інтерфейсу IReader.cs

---
+++ Library.Core/Interfaces/IReader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Library.Core
+{
+    /// <summary>
+    /// Інтерфейс для роботи з читачами
+    /// </summary>
+    public interface IReader
+    {
+        /// <summary>
+        /// Зареєструвати нового читача
+        /// </summary>
+        void Register(string name, string email);
+
+        /// <summary>
+        /// Змінити дані читача
+        /// </summary>
+        void Change(string name, string email);
+
+        /// <summary>
+        /// Взяти книгу з бібліотеки
+        /// </summary>
+        void BorrowBook(IBook book);
+
+        /// <summary>
+        /// Повернути книгу в бібліотеку
+        /// </summary>
+        void ReturnBook(IBook book);
+
+        /// <summary>
+        /// Отримати інформацію про читача
+        /// </summary>
+        string InfoReader();
+
+        /// <summary>
+        /// Отримати інформацію про книги читача
+        /// </summary>
+        string AboutBookOfReader();
+    }
+}
